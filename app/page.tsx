5fd3830071d205bb9064b5f4eb5e3fb973fd1f8f
Kunal Patel <kunal_patel200@hotmail.co.uk>
2024-03-15 15:59:00 +0000

    feat(about): develop 'Who Are We?' section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
+import Image from "next/image";
 import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <main>
       <div className={styles.hero}></div>
+      <div className={styles.mainContainer}>
+
+        <section className={styles.aboutContainer}>
+          <div>
+            <h1>Who Are We?</h1>
+            <div className={styles.underline}></div>
+          </div>
+          <p>George King who trained himself as a Master of Yoga was used by Higher Forces to act as a channel for Truth to be given to humanity prior to the dawn of the Aquarian Age of Peace and Enlightenment. The Six Adepts carried out battles in the lower astral realms to rid the worst devil that had existed there.</p>
+          <p>The Association&apos;s function is to promote the work of the Six Adepts and carry out tasks as laid down to help bring lasting Peace and prosperity on Earth.</p>
+          <p>Those wishing to join need to have a burning desire to work for World Peace. They need to begin by studying the work and sacrifice made by Doctor George King and the Mighty Six Adepts.</p>
+          <Image
+            className={styles.aboutImg}
+            src={"/about-img.png"}
+            width={250}
+            height={338}
+            alt="George King"
+          />
+        </section>
+
+      </div>
     </main>
   );
 }
