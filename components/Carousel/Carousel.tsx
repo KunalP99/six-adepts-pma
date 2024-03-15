70d3710cf171e77b0e44fdc61f817f8b5bc92999
Kunal Patel <kunal_patel200@hotmail.co.uk>
2024-03-15 18:32:51 +0000

    feat: add Carousel component and style it

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,17 +1,37 @@
 "use client"
 import useEmblaCarousel from "embla-carousel-react";
 import styles from "./carousel.module.scss";
+import Image from "next/image";
+import { cormorant } from "@/app/font";
 
-const Carousel = () => {
+interface ISingleArticle {
+  img: string,
+  alt: string,
+  title: string,
+  summary: string
+}
+
+const Carousel = ({ img, alt, title, summary }: ISingleArticle) => {
   const [emblaRef] = useEmblaCarousel({ loop: false });
 
   return (
     <div className={styles.embla} ref={emblaRef}>
       <div className={styles.emblaContainer}>
-        <div className={styles.emblaSlide}>Slide 1</div>
-        <div className={styles.emblaSlide}>Slide 2</div>
-        <div className={styles.emblaSlide}>Slide 3</div>
+        <div className={styles.singleArticle}>
+          <Image
+            src={"/articles/pyramid-exploration.png"}
+            width={220}
+            height={187}
+            alt="Pyramid Exploration article"
+          />
+          <div>
+            <h3 className={cormorant.className}>Pyramid Exploration</h3>
+            <div className={styles.underline}></div>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
       </div>
+
     </div>
   )
 }
