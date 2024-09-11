7c9ab9f4cd2cd69413109927ad66b08ac689c268
Kunal Patel <kunal_patel200@hotmail.co.uk>
2024-09-11 13:40:06 +0100

    feat: add article page, change hero page

---
+++ app/articles/page.tsx
@@ -0,0 +1,9 @@
+import styles from './articles.module.scss';
+
+export default function Articles() {
+  return (
+    <div className={styles.articlesContainer}>
+      <h1 className={styles.articleHeading}>Articles</h1>
+    </div>
+  )
+}
