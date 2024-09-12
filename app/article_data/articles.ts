c6a18950f6b9248957f0276a078d1eeb3f2afb64
Kunal Patel <kunal_patel200@hotmail.co.uk>
2024-09-12 17:23:39 +0100

    feat: add images to articles

--- app/article_data/articles.ts
+++ app/article_data/articles.ts
@@ -5,7 +5,7 @@ const article = [
     img: "background-gk.png",
     summary: "George King & Six Adepts Private Members Association is set up as a world-wide religious organisation based entirely on the work and legacy of the Englishman – the late Doctor George King (23rd January 1919 to 12th July 1997)",
     section: "history-and-background",
-    url: "https://ia600800.us.archive.org/24/items/background-to-george-king-and-six-adepts-pma/Background%20to%20George%20King%20and%20Six%20Adepts%20PMA.pdf"
+    url: "https://ia800103.us.archive.org/11/items/background-to-george-king-and-six-adepts-pma_202409/Background%20to%20George%20King%20and%20Six%20Adepts%20PMA.pdf"
   },
   {
     id: 2,
@@ -13,12 +13,12 @@ const article = [
     img: "",
     summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     section: "history-and-background",
-    url: ""
+    url: "https://ia600103.us.archive.org/0/items/six-cosmic-knights/Six%20Cosmic%20Knights.pdf"
   },
   {
     id: 3,
     title: "Armageddon ",
-    img: "background-gk.png",
+    img: "armageddon.png",
     summary: "Throughout the ages there has been talk of Armageddon – the bitter clash between good and evil.  Is there a basis for such a concept?  Or is it some mysterious unknown phenomena best left alone.",
     section: "missions-carried-out-by-six-adepts",
     url: "https://ia902306.us.archive.org/21/items/armageddon_202409/Armageddon.pdf"
@@ -29,7 +29,7 @@ const article = [
     img: "sunbeam.png",
     summary: "Most people on Earth are not aware of the Galactic-Shattering missions carried out by Master George King and the Mighty Six Adepts in the last century.  Only on a sleepy, dopy, ignorant planet, such as ours, could such events go un-noticed.",
     section: "missions-carried-out-by-six-adepts",
-    url: ""
+    url: "https://ia600101.us.archive.org/29/items/operation-sunbeam-and-space-magic/Operation%20Sunbeam%20and%20Space%20Magic.pdf"
   },
   {
     id: 5,
@@ -37,7 +37,7 @@ const article = [
     img: "pyramid-enigma.png",
     summary: "Many decades ago what had begun as a mild interest in the mystery of the pyramids grew in stature as the years rolled on.  Brought up in an eastern environment and educated in the western sciences I did not let go of either of those traits under the illusion that one was right and the other wrong or lacking in logic.",
     section: "aquarian-age-sciences-and-developments",
-    url: ""
+    url: "https://ia902301.us.archive.org/23/items/the-pyramid-enigma/The%20Pyramid%20Enigma.pdf"
   },
   {
     id: 6,
@@ -45,7 +45,7 @@ const article = [
     img: "visit-to-saqqara.png",
     summary: "On the last day of my visit to Egypt, I decided to spend time examining some of the other pyramids which are not very far from the Giza Pyramids.  On making enquiries, my guide suggested that we should travel south to the Step Pyramid of Saqqara which according to archaeologists are even older than the Giza Group!",
     section: "aquarian-age-sciences-and-developments",
-    url: ""
+    url: "https://ia902305.us.archive.org/32/items/visit-to-saqqara/Visit%20to%20Saqqara.pdf"
   },
   {
     id: 7,
@@ -53,7 +53,7 @@ const article = [
     img: "pyramid-exploration.png",
     summary: "Over the ensuing years seminars and gatherings have been held to promote and re-vitalise the whole aspect of the mystery behind the pyramids.  From the experiences of those taking part what emerges is a picture of the make-up and application of these machines in the days gone by which is beyond the grasp of the material sciences and mental capabilities of the present age.",
     section: "aquarian-age-sciences-and-developments",
-    url: ""
+    url: "https://ia600601.us.archive.org/24/items/pyramid-exploration/Pyramid%20Exploration.pdf"
   }
 ]
 
